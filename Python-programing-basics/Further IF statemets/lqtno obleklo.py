0415d5167b86df218a45b90abc3748ca9738b9bd
vanch1n1 <75495072+vanch1n1@users.noreply.github.com>
2020-12-29 16:17:34 +0200

    More code

---
+++ Python-programing-basics/Further IF statemets/lqtno obleklo.py
@@ -0,0 +1,10 @@
+degrees = float(input()
+time_from_the_day = input()
+if 10 <= degrees <= 18:
+    if time_from_the_day == 'Morning':
+       Outfit = 'Sweatshirt'
+       Shoes = 'Sneakers'
+    if time_from_the_day == 'Afternoon':
+       outfit = 'Shirt'
+       shoes = 'Moccasins'
+    if time_from_the_day =
